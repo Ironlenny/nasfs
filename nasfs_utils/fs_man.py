aed8a700dbb6f545f4eb70b792392454260933a8
Jacob Riddle <ironlenny@gmail.com>
2016-10-06 18:25:52 -0700

    Implementing database creation

--- nasfs_utils/fs_man.py
+++ nasfs_utils/fs_man.py
@@ -1,4 +1,7 @@
+#!/usr/bin/python3
+
 import bson
+import bsddb3
 
 class FSMan():
 
@@ -6,13 +9,14 @@ class FSMan():
         self._db_name = "meta_db"
         self._super_file = "super_file"
 
-    def _create_meta_db(self):
-        return
-
     def create_fs(self, vol, raidLv):
         file = open(self._super_file, 'bx')
         file.write(bson.dumps({"vol":vol, "raid":raidLv}))
         file.close()
+        db = bsddb3.db.open(self._db_name, None, bsddb3.db.DB_BTREE,\
+                            bsddb3.db.DB_CREATE)
+        db.put('root', 'self')
+        db.close()
 
     def add_vol(self, mount, vol):
         return
