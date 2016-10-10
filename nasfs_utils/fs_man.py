2448324e318a56172bce80d8822dbde9b8e5fefc
Jacob Riddle <ironlenny@gmail.com>
2016-10-09 19:24:25 -0700

    Add meta database creation

--- nasfs_utils/fs_man.py
+++ nasfs_utils/fs_man.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+# TODO: Disallow duplicates in the database
 import bson
 import bsddb3
 
@@ -8,18 +8,27 @@ class FSMan():
     def __init__(self):
         self._db_name = "meta_db"
         self._super_file = "super_file"
+        self.db = bsddb3.db.DB()
 
-    def create_fs(self, vol, raidLv):
+    def create_fs(self, vol, raidLv, uid, gid, perm):
+        root_rec = {'contains': ['.', '..'], 'uid': uid, 'gid': gid,\
+                     'perm': perm, 'atime': None, 'dir': True}
         file = open(self._super_file, 'bx')
         file.write(bson.dumps({"vol":vol, "raid":raidLv}))
         file.close()
-        db = bsddb3.db.open(self._db_name, None, bsddb3.db.DB_BTREE,\
-                            bsddb3.db.DB_CREATE)
-        db.put('root', 'self')
-        db.close()
+
+        self.db.open(self._db_name, '1', bsddb3.db.DB_BTREE,\
+                bsddb3.db.DB_CREATE)
+        self.db.put(b'self', bson.dumps(root_rec))
+        self.db.close()
 
     def add_vol(self, mount, vol):
-        return
+        file = open( mount + self._super_file, 'br+')
+        data = bson.loads(file.read())
+        data['vol'].append(vol)
+        file.seek(0)
+        file.write(bson.dumps(data))
+        file.close()
 
     def remove_vol(self, mount, vol):
         return
