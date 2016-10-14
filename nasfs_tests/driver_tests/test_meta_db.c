231866066f60a1854b96942742742450c2e8a765
Jacob Riddle <ironlenny@gmail.com>
2016-10-14 16:41:11 -0700

    test_meta_db.c segfaults

--- nasfs_tests/driver_tests/test_meta_db.c
+++ nasfs_tests/driver_tests/test_meta_db.c
@@ -1,15 +1,20 @@
 #include <tap.h>
 #include "../../nasfs_driver/meta_db.h"
 #include <stdbool.h>
+#include <lmdb.h>
+#include <sys/stat.h>
 
-const char db_file[] = "meta_db";
-DB *db;
+const char db_file[] = "./meta_db";
+MDB_env *env;
+MDB_dbi *dbi;
+int result = 0;
 
 int main()
 {
   plan(NO_PLAN);
+  mkdir(db_file, 0777);
+  result = meta_open(db_file, 0, false, &env, dbi);
+  cmp_ok(result , "==", 0, "Create db file");
 
-  todo();
-  ok(meta_open(db_file, '1', true), "Create db file, and root logical db");
-  end_todo;
+  return 0;
 }
