9f1c00d8032e8870aeb9af441fb491593dcfeaed
Jacob Riddle <ironlenny@gmail.com>
2016-10-25 10:41:11 -0700

    Add tests for creating a named db and opening a named db

--- nasfs_tests/driver_tests/test_meta_db.c
+++ nasfs_tests/driver_tests/test_meta_db.c
@@ -5,16 +5,26 @@
 #include <sys/stat.h>
 
 const char db_file[] = "./meta_db";
-MDB_env *env;
-MDB_dbi *dbi;
+MDB_env *env = NULL;
+MDB_dbi db_handle;
 int result = 0;
+MDB_txn *txn = NULL;
+
+int test_open(bool create, unsigned long db_id, int max_db)
+{
+  return meta_open(db_file, db_id, create, &env, &db_handle, max_db, txn);
+}
 
 int main()
 {
   plan(NO_PLAN);
   mkdir(db_file, 0777);
-  result = meta_open(db_file, 0, false, &env, dbi);
-  cmp_ok(result , "==", 0, "Create db file");
-
+  result = test_open(false, 0, 0);
+  cmp_ok(result , "==", 0, "Create and open db file");
+  result = test_open(true, 1, 10);
+  cmp_ok(result , "==", 0, "Create and open named db");
+  mdb_txn_commit(txn);
+  result = test_open(false, 1, 10);
+  cmp_ok(result , "==", 0, "Open named db");
   return 0;
 }
