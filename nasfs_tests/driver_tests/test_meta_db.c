d4c9b907815423fcdc20e05a157a80c8b30f7e69
Jacob Riddle <ironlenny@gmail.com>
2016-10-26 18:46:42 -0700

    Add meta_get() and meta_put() tests.

--- nasfs_tests/driver_tests/test_meta_db.c
+++ nasfs_tests/driver_tests/test_meta_db.c
@@ -8,11 +8,17 @@ const char db_file[] = "./meta_db";
 MDB_env *env = NULL;
 MDB_dbi db_handle;
 int result = 0;
-MDB_txn *txn = NULL;
+char *meta_value;
+
 
 int test_open(bool create, unsigned long db_id, int max_db)
 {
-  return meta_open(db_file, db_id, create, &env, &db_handle, max_db, txn);
+  return meta_open(db_file, db_id, create, &env, &db_handle, max_db);
+}
+
+void test_close()
+{
+  meta_close(&env, &db_handle);
 }
 
 int main()
@@ -21,10 +27,16 @@ int main()
   mkdir(db_file, 0777);
   result = test_open(false, 0, 0);
   cmp_ok(result , "==", 0, "Create and open db file");
+  test_close();
   result = test_open(true, 1, 10);
   cmp_ok(result , "==", 0, "Create and open named db");
-  mdb_txn_commit(txn);
+  test_close();
   result = test_open(false, 1, 10);
   cmp_ok(result , "==", 0, "Open named db");
+  cmp_ok(meta_put(&env, &db_handle, "foo", "bar"), "==", 0, "Wrote \"foo\".");
+  cmp_ok(meta_get(&env, &db_handle, "foo", meta_value), "==", 0,
+         "Read \"foo\".");
+  is(meta_value, "bar");
+
   return 0;
 }
