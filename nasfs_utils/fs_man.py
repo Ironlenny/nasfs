58f17241dc4b1673efb4fdee8a65769a67f94d0d
Jacob Riddle <ironlenny@gmail.com>
2016-12-08 11:55:06 -0800

    Change super_file to super_block
    Changed to have more consistent terminology with existing file systems.

--- nasfs_utils/fs_man.py
+++ nasfs_utils/fs_man.py
@@ -7,21 +7,21 @@ class FSMan():
 
     def __init__(self):
         self._db_name = 'meta_db'
-        self._super_file = 'super_file'
+        self._super_block = 'super_block'
         self.db = lmdb.open(self._db_name)
 
     def create_fs(self, vol, raidLv, uid, gid, perm, max_dir):
         root_rec = {'contains': ['.', '..'], 'uid': uid, 'gid': gid,\
                      'perm': perm, 'atime': None, 'dir': True}
-        file = open(self._super_file, 'bx')
-        file.write(bson.dumps({"vol":vol, "raid":raidLv, "max_dir":max_dir}))
+        file = open(self._super_block, 'bx')
+        file.write(bson.dumps({"vol":vol, "raid_lv":raidLv, "max_dir":max_dir}))
         file.close()
 
         with self.db.begin(write=True) as txn:
             txn.put(b'self', bson.dumps(root_rec))
 
     def add_vol(self, mount, vol):
-        file = open( mount + self._super_file, 'br+')
+        file = open( mount + self._super_block, 'br+')
         data = bson.loads(file.read())
         data['vol'].append(vol)
         file.seek(0)
