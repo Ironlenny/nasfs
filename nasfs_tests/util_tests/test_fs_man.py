0172bdf0e873d8bb13f76c0b36bf5355ec309782
Jacob Riddle <ironlenny@gmail.com>
2017-02-17 16:39:50 -0800

    fs_man.py, test_fs_man.py: Convert to using msgpack
    Messagepack is a serialization format better suited to the needs of
    NASFS. Replace 'bson' with 'msgpack', and remove support for LMDB.
    Meta-data will be stored directly on the underlying filesystem.

--- nasfs_tests/util_tests/test_fs_man.py
+++ nasfs_tests/util_tests/test_fs_man.py
@@ -1,63 +1,47 @@
-import lmdb
 import pytest
 import sys
 sys.path.append ("../../nasfs_utils/")
 import fs_man
-import bson # Do not use debian version. Use https://github.com/py-bson/bson
 import os
 import shutil
+import msgpack
 
-super_file = 'super_file'
-raidLv = 1
-dev = ['/dev/sda1']
-super_dict = {'vol': dev, 'raid': raidLv}
+super_file = 'super_block'
+raid_lv = 1
+dev = [b'/dev/sda1']
+super_dict = {b'vol': dev, b'raid_lv': raid_lv}
 mount = './'
-meta_db = 'meta_db'
-uid = 'test'
-gid = 'test'
+uid = b'test'
+gid = b'test'
 perm = 772
 
 def _open_super_file():
     file = open(super_file, 'rb')
-    return bson.loads(file.read())
+    return msgpack.loads(file.read())
 
 def _cleanup():
     if os.path.isfile(super_file):
         os.remove(super_file)
 
-    if os.path.isdir(meta_db):
-        shutil.rmtree(meta_db)
-
 def test_create_super_file():
     _cleanup()
     test_obj = fs_man.FSMan()
-    test_obj.create_fs(dev, raidLv, uid, gid, perm)
-    bson_dict = _open_super_file()
-    assert super_dict == bson_dict
+    test_obj.create_fs(dev, raid_lv, uid, gid, perm)
+    msgpack_dict = _open_super_file()
+    assert super_dict == msgpack_dict
     with pytest.raises(FileExistsError):
-        test_obj.create_fs(dev, raidLv, uid, gid, perm)
-
-def test_create_meta_db():
-    _cleanup()
-    test = {'contains': ['.', '..'], 'uid': uid, 'gid': gid, 'perm': perm\
-            , 'atime': None, 'dir': True}
-    test_obj = fs_man.FSMan()
-    test_obj.create_fs(dev, raidLv, uid, gid, perm)
-    test_db = lmdb.open(meta_db)
-    with test_db.begin() as txn:
-        rec = txn.get(b'self')
-    assert test == bson.loads(rec)
+        test_obj.create_fs(dev, raid_lv, uid, gid, perm)
 
 def test_add_volume():
     _cleanup()
     test_obj = fs_man.FSMan()
-    test_obj.create_fs(dev, raidLv, uid, gid, perm)
+    test_obj.create_fs(dev, raid_lv, uid, gid, perm)
     test_dict = super_dict
-    vol = '/dev/sdb1'
-    test_dict['vol'].append(vol)
+    vol = b'/dev/sdb1'
+    test_dict[b'vol'].append(vol)
     test_obj.add_vol(mount, vol)
-    bson_dict = _open_super_file()
-    assert test_dict == bson_dict
+    msgpack_dict = _open_super_file()
+    assert test_dict == msgpack_dict
 
 def test_cleanup():
     _cleanup()
--- nasfs_utils/fs_man.py
+++ nasfs_utils/fs_man.py
@@ -1,31 +1,26 @@
 #!/usr/bin/python3
 import pdb
-import bson # Do not use debian version. Use https://github.com/py-bson/bson
-import lmdb
+import msgpack
+
 
 class FSMan():
 
     def __init__(self):
-        self._db_name = 'meta_db'
         self._super_block = 'super_block'
-        self.db = lmdb.open(self._db_name)
 
-    def create_fs(self, vol, raidLv, uid, gid, perm, max_dir):
+    def create_fs(self, vol, raidLv, uid, gid, perm):
         root_rec = {'contains': ['.', '..'], 'uid': uid, 'gid': gid,\
                      'perm': perm, 'atime': None, 'dir': True}
         file = open(self._super_block, 'bx')
-        file.write(bson.dumps({"vol":vol, "raid_lv":raidLv, "max_dir":max_dir}))
+        file.write(msgpack.dumps({"vol":vol, "raid_lv":raidLv}))
         file.close()
-
-        with self.db.begin(write=True) as txn:
-            txn.put(b'self', bson.dumps(root_rec))
 
     def add_vol(self, mount, vol):
         file = open( mount + self._super_block, 'br+')
-        data = bson.loads(file.read())
-        data['vol'].append(vol)
+        data = msgpack.loads(file.read())
+        data[b'vol'].append(vol)
         file.seek(0)
-        file.write(bson.dumps(data))
+        file.write(msgpack.dumps(data))
         file.close()
 
     def remove_vol(self, mount, vol):
