97b80ce52413658dee7dd23ad25f9dcb74f7c6e2
Jacob Riddle <ironlenny@gmail.com>
2017-05-24 13:46:41 -0700

    test_nasfs.c: Add directory contents test

--- nasfs_tests/driver_tests/test_nasfs.c
+++ nasfs_tests/driver_tests/test_nasfs.c
@@ -1,24 +1,31 @@
 #include <tap.h> /* https://github.com/zorgnax/libtap */
 #include "../../nasfs_driver/nasfs.c"
-#include "../../nasfs_driver/mpack.h"
+#include "../../lib/mpack.h"
 #include <fuse.h>
 #include <stdlib.h>
+#include "../../lib/sds.h"
 
 static char super_path[] = "./super_block";
-static char root_path[] = "./root";
+static char root_path[] = "./test_root";
 
 void test_mp_load(char *path)
 {
   Inode *node = NULL;
   char *test_contents[2];
-  test_contents[0] = ".";
-  test_contents[1] = "..";
   cmp_ok(node = mp_load(path), "!=" , NULL , "inode created");
   if (node != NULL)
     {
       is(node->name, "root" , "Node is named root");
       cmp_ok(node->dir, "==" , true , "Node is a directory");
       cmp_ok(node->uid, "==" , 1 , "uid is 1");
+      cmp_ok(node->dir_size, "==", 4, "dir_size is 4");
+      for (int i = 0; i < node->dir_size; i++)
+        {
+          sds string = sdsnew("foo");
+          string = sdscat(string, sdsfromlonglong(i));
+          is(node->contents_a[i], string,  "Contents match");
+          sdsfree(string);
+        }
     }
 }
 
@@ -30,3 +37,4 @@ int main(int argc, const char *argv[])
 
   return 0;
 }
+
