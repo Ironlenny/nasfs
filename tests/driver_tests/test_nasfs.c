d09287da3d6575f7e63c81e67f56d5b4ded42544
Jacob Riddle <ironlenny@gmail.com>
2017-06-01 13:20:57 -0700

    Add 'mp_store()', 'nas_mkdir()', and 'nas_rmdir()' tests

--- tests/driver_tests/test_nasfs.c
+++ tests/driver_tests/test_nasfs.c
@@ -1,24 +1,26 @@
 #include <tap.h> /* https://github.com/zorgnax/libtap */
-#include "../../nasfs_driver/nasfs.c"
+#include "../../src/nasfs.c"
 #include "../../lib/mpack.h"
 #include <fuse.h>
 #include <stdlib.h>
 #include "../../lib/sds.h"
 
 static char super_path[] = "./super_block";
-static char root_path[] = "./test_root";
+char root_path[] = "./test_root";
+Inode *node = NULL;
 
 void test_mp_load(char *path)
 {
-  Inode *node = NULL;
-  char *test_contents[2];
-  cmp_ok(node = mp_load(path), "!=" , NULL , "inode created");
+  printf("Try to load MessagePack file and create inode:\n");
+  cmp_ok(mp_load(path, &node), "==" , 0 , "inode created");
   if (node != NULL)
     {
       is(node->name, "root" , "Node is named root");
       cmp_ok(node->dir, "==" , true , "Node is a directory");
       cmp_ok(node->uid, "==" , 1 , "uid is 1");
       cmp_ok(node->dir_size, "==", 4, "dir_size is 4");
+      cmp_ok(node->perm, "==", 777, "perm is 777");
+      cmp_ok(node->ctime, "==", 1494872263, "ctime is 1494872263");
       for (int i = 0; i < node->dir_size; i++)
         {
           sds string = sdsnew("foo");
@@ -29,12 +31,24 @@ void test_mp_load(char *path)
     }
 }
 
+void test_mp_store()
+{
+  printf("Try to write inode to file:\n");
+  if(node == NULL)
+    {
+      fail("Cannot store in file. 'node' is NULL");
+      return;
+    }
+  cmp_ok(mp_store(node, root_path), "==", 0, "Store successful");
+}
+
 int main(int argc, const char *argv[])
 {
   printf("nasfs.c tests:\n");
   plan(NO_PLAN);
   test_mp_load(root_path);
-
+  test_mp_store();
+  cmp_ok(nas_mkdir("foo", 0777), "==", 0, "Made directory 'foo'");
+  cmp_ok(nas_rmdir("foo"), "==", 0, "Removed directory 'foo'");
   return 0;
 }
-
